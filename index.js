c093b82979a0d7468be5af69244b62fb1c91ce53
Tom Murray <tomkeohanemurray@gmail.com>
2018-08-19 00:22:19 +0700

    added more sockets

--- index.js
+++ index.js
@@ -9,15 +9,36 @@ app.use(bodyParser.json());
 
 app.post('/order-created', function(req, res){
   // sending to all connected clients
+  console.log('/order-created');
   console.log(req.body);
   io.emit('order-created', req.body.id);
+  for(let broker of req.body.brokers) {
+    io.emit(`order-created-broker:${broker}`, req.body.id);
+  }
   return res.json({message: 'Finished'});
 });
 
 app.post('/order-set-price', function(req, res){
   // sending to all connected clients
+  console.log('/order-set-price');
   console.log(req.body);
   io.emit('order-set-price', req.body.id);
+  return res.json({message: 'Finished'});
+});
+
+app.post('/order-investor-confirm', function(req, res){
+  // sending to all connected clients
+  console.log('/order-investor-confirm');
+  console.log(req.body);
+  io.emit('order-investor-confirm', req.body.id);
+  return res.json({message: 'Finished'});
+});
+
+app.post('/order-broker-confirm', function(req, res){
+  // sending to all connected clients
+  console.log('/order-broker-confirm');
+  console.log(req.body);
+  io.emit('order-broker-confirm', req.body.id);
   return res.json({message: 'Finished'});
 });
 
