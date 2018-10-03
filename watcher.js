f898f711f4edcfd27857d4a079f72b6811b9fd1a
Tom Murray <tomkeohanemurray@gmail.com>
2018-10-03 08:21:02 +0100

    upgraded to web3@1

--- watcher.js
+++ watcher.js
@@ -8,8 +8,10 @@ const provider = process.env.PROVIDER || 'http://127.0.0.1:8545'
 const api_url = process.env.API_URL || 'http://localhost:8000'
 const promisify = require('tiny-promisify')
 
-const web3 = new Web3(new Web3.providers.HttpProvider(provider));
-
+let web3 = provider.split(':')[0] === 'http'
+? new Web3(new Web3.providers.HttpProvider(provider))
+: new Web3(new Web3.providers.WebsocketProvider(provider))
+// const web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8546'))
 let tradeKernelEvents = [
   {name: 'LogConfirmed', api_url_end: 'trades/confirmed', api_type: 'PUT'},
   {name: 'LogCancel', api_url_end: 'trades/cancel', api_type: 'PUT'},
@@ -24,11 +26,26 @@ let ettEvents = [
   {name: 'EndOfDay', api_url_end: 'tokens/contract/end-of-day', api_type: 'PUT'},
 ];
 
-let watchCallback = (api_url_end, api_type, extra_args={}) => async (err, event) => {
+let getContracts = async () => {
+  let TradeKernel, TokenFactory, ETT;
+  if(isUrl(contract_folder)) {
+    TradeKernel = await rp.get(`${contract_folder}TradeKernel.json`, {json:true});
+    TokenFactory = await rp.get(`${contract_folder}TokenFactory.json`, {json:true});
+    ETT = await rp.get(`${contract_folder}ETT.json`, {json:true});
+  }else{
+    TradeKernel = require(`${contract_folder}TradeKernel.json`);
+    TokenFactory = require(`${contract_folder}TokenFactory.json`);
+    ETT = require(`${contract_folder}ETT.json`);
+  }
+  return {TradeKernel, TokenFactory, ETT};
+}
+
+
+let watchCallback = (api_url_end, api_type, extra_args={}) => async (event) => {
   api_type = api_type ? api_type : 'POST'
-  let args = event.args
+  let args = event.returnValues
   let api_url_end_clone = api_url_end;
-  for(let key of Object.keys(event.args)) {
+  for(let key of Object.keys(event.returnValues)) {
     if(args[key].constructor.name == 'BigNumber') args[key] = args[key].toNumber()
     if(api_url_end.includes(`{${key}}`)) api_url_end_clone = api_url_end.replace(`{${key}}`, args[key])
   }
@@ -42,78 +59,48 @@ let watchCallback = (api_url_end, api_type, extra_args={}) => async (err, event)
   }
 }
 
-let getContracts = async () => {
-  let network = await promisify(web3.version.getNetwork)()
-  let TradeKernel, TokenFactory, ETT;
-  if(isUrl(contract_folder)) {
-    TradeKernel = await rp.get(`${contract_folder}TradeKernel.json`, {json:true});
-    TokenFactory = await rp.get(`${contract_folder}TokenFactory.json`, {json:true});
-    ETT = await rp.get(`${contract_folder}ETT.json`, {json:true});
-  }else{
-    TradeKernel = require(`${contract_folder}TradeKernel.json`);
-    TokenFactory = require(`${contract_folder}TokenFactory.json`);
-    ETT = require(`${contract_folder}ETT.json`);
-  }
-  const tradeKernel = contract(TradeKernel);
-  tradeKernel.setProvider(web3.currentProvider);
-  const tokenFactory = contract(TokenFactory);
-  tokenFactory.setProvider(web3.currentProvider);
-  const ett = contract(ETT);
-  ett.setProvider(web3.currentProvider);
-  return {tradeKernel, tokenFactory, ett};
-}
-
-let createTokenWatch = (ett) => (err, event) => {
-  console.log(err)
-  console.log(event)
-  if(err){
-    return console.log(err)
-  }
-  let {tokenAddress} = event.args
-  ett.at(tokenAddress)
-  .then((instance) => {
-    for(let event of ettEvents) {
-      if(instance[event.name]){
-        instance[event.name]({},{fromBlock: 0, toBlock: 'pending'})
-        .watch(watchCallback(event.api_url_end, event.api_type, {token: tokenAddress}));
-      }else{
-        console.log(`No event for ${event.name}`);
-      }
+let createTokenWatch = (ETT) => (event) => {
+  let {tokenAddress} = event.returnValues
+  let ett = new web3.eth.Contract(ETT.abi, tokenAddress)
+  for(let event of ettEvents) {
+    if(instance[event.name]){
+      instance[event.name]({},{fromBlock: 0, toBlock: 'pending'})
+      .watch(watchCallback(event.api_url_end, event.api_type, {token: tokenAddress}));
+    }else{
+      console.log(`No event for ${event.name}`);
     }
-  })
+  }
 }
 
 let watch = async () => {
-  let {tradeKernel, tokenFactory, ett} = await getContracts();
-  web3.eth.getAccounts((err, accounts) => {
-    tradeKernel.deployed()
-    .then((instance) => {
-      for(let event of tradeKernelEvents) {
-        if(instance[event.name]){
-          instance[event.name]({},{fromBlock: 0, toBlock: 'pending'})
-          .watch(watchCallback(event.api_url_end, event.api_type));
-        }else{
-          console.log(`No event for ${event.name}`);
-        }
-      }
+  let network = await web3.eth.net.getId()
+  let {TradeKernel, TokenFactory, ETT} = await getContracts();
+  let tradeKernel = new web3.eth.Contract(TradeKernel.abi, TradeKernel.networks[`${network}`].address)
+  let tokenFactory = new web3.eth.Contract(TokenFactory.abi, TokenFactory.networks[`${network}`].address)
+  for(let event of tradeKernelEvents) {
+    tradeKernel.events[event.name]({
+      fromBlock: 0
     })
-    .catch((err) => {
-      console.log(err);
+    .on('data', function(data){
+        return watchCallback(event.api_url_end, event.api_type)(data)
     })
-    tokenFactory.deployed()
-    .then((instance) => {
-      instance.TokenCreated({},{fromBlock: 0, toBlock: 'pending'})
-      .watch((err, event) => {
-        console.log('tokens/contract/update')
-        watchCallback('tokens/contract/update','PUT')(err, event)
-        createTokenWatch(ett)(err, event)
-      });
+    .on('changed', function(event){
+        // remove event from local database
     })
-    .catch((err) => {
-      console.log(err);
-    })
+    .on('error', console.error);
+  }
 
+  tokenFactory.events.TokenCreated({
+      fromBlock: 0
   })
+  .on('data', function(data){
+    console.log(data.returnValues)
+    watchCallback('tokens/update/contract','PUT')(data)
+  })
+  .on('changed', function(event){
+      // remove event from local database
+  })
+  .on('error', console.error);
 }
 
 module.exports = {
